3909ae518d802b89cfead5fd897c144d47d180dc
bob <eanbo@lzb.ac.cn>
2016-10-08 12:04:14 +0800

    + ModelWaterUseEfficiencyAgri

---
+++ src/main/java/com/smates/dbc2/model/ModelWaterUseEfficiencyAgri.java
@@ -0,0 +1,43 @@
+package com.smates.dbc2.model;
+
+/**
+ * @class 
+ * @brief 农业水利用效率 
+ * @author Bob, Ge Yingchun
+ * @version 
+ * @date 
+ * @note
+ *  WuE = Ea/Qa  
+ *  其中，WuE 为农业水利用效率，
+ *    Ea为作物总蒸腾量，m3
+ *    Qa为农业总用水量，m3
+ */
+public class ModelWaterUseEfficiencyAgri {
+
+	public ModelWaterUseEfficiencyAgri(){
+		
+	}
+	
+	/**
+	 * 
+	 * @param et	double	作物总蒸腾量，m3
+	 * @param qa	double	农业总用水量，m3
+	 * @return wue	double	 农业水利用效率
+	 * @exception 
+     * @note
+     *     农业水利用效率 = 作物总蒸腾量/农业总用水量
+	 */
+	public double calc( double et, double qt){
+
+		double wue = 0;
+		if(qt!=0)
+			wue = et/qt;
+		return wue;
+	}
+	
+
+	/** @brief brief description  */
+	public void init(){
+	}
+
+}
