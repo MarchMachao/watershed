9edbb7e68a0b7eb03984249ffd3d540ee8c6d9f9
baijw <295188526@qq.com>
2016-10-09 21:39:56 +0800

    没有默认值的选项readonly，树状图接口写好，绘图还有问题

--- src/main/java/com/smates/dbc2/service/impl/GoalServiceImpl.java
+++ src/main/java/com/smates/dbc2/service/impl/GoalServiceImpl.java
@@ -36,10 +36,22 @@ public class GoalServiceImpl implements GoalService {
 	public Node getGoalTreeById(String id) {
 		Node root = new Node();
 		Node node1 = new Node();
+	
 		Node node11 = null;
 		Node node12 = null;
 		Node node13 = null;
 		Node node14 = null;
+		Node node21 = null;
+		Node node22 = null;
+		Node node23 = null;
+		Node node31 = null;
+		Node node32 = null;
+		Node node33 = null;
+		Node node34 = null;
+		Node node35 = null;
+		Node node36 = null;
+		Node node37 = null;
+		Node node38 = null;
 		Goal goal = goalDao.getGoalById(id);
 		node1.setName("为所有人提供水和环境卫生，为所有用水部门提供可持续的供水和取水，并进行流域水资源的可持续管理"+goal.getGoal_1()+"%");
 		List<Node> node1child = new ArrayList<Node>();
@@ -112,11 +124,158 @@ public class GoalServiceImpl implements GoalService {
 		node1.setChildren(node1child);
 		
 		
+		Node node2 = new Node();
+		node2.setName("保护、恢复和促进流域可持续利用陆地生态系统健康发展，可持续管理森林，防治荒漠化"+goal.getGoal_2()+"%");
+		List<Node> node2child = new ArrayList<Node>();
+		String index_211 = goal.getIndex_211();
+		if (!StringUtils.isEmpty(index_211)) {
+			node21 = new Node();
+			node21.setName(" 提高水资源利用率");
+			List<Node> children = new ArrayList<Node>();
+			children.add(new Node("森林覆盖率"+index_211+"%", null));
+			node21.setChildren(children);
+			node2child.add(node21);
+		}
+		String index_221 = goal.getIndex_221();
+		if (!StringUtils.isEmpty(index_221)) {
+			node22 = new Node();
+			node22.setName("促进所有类型森林可持续管理的实施");
+			List<Node> children = new ArrayList<Node>();
+			children.add(new Node("可持续森林管理覆盖"+index_221+"%", null));
+			node22.setChildren(children);
+			node2child.add(node22);
+		}
+		String index_231 = goal.getIndex_231();
+		if (!StringUtils.isEmpty(index_231)) {
+			node23 = new Node();
+			node23.setName("确保山地生态系统的可持续发展");
+			List<Node> children = new ArrayList<Node>();
+			children.add(new Node("山地绿色覆盖指数"+index_231+"%", null));
+			node23.setChildren(children);
+			node2child.add(node23);
+		}
+		node2.setChildren(node2child);
 		
 		
 		
+		Node node3 = new Node();
+		node3.setName("促进持久、包容性和可持续经济增长、促进实现充分和生产性就业"+goal.getGoal_3()+"%");
+		List<Node> node3child = new ArrayList<Node>();
+		String index_311 = goal.getIndex_311();
+		if (!StringUtils.isEmpty(index_311)) {
+			node31 = new Node();
+			node31.setName(" 可持续与自然环境相一致的人均经济增长");
+			List<Node> children = new ArrayList<Node>();
+			children.add(new Node("人均GDP"+index_311+"元", null));
+			node31.setChildren(children);
+			node3child.add(node31);
+		}
+		String index_321 = goal.getIndex_321();
+		if (!StringUtils.isEmpty(index_321)) {
+			node32 = new Node();
+			node32.setName(" 得到经济生产力的高水平");
+			List<Node> children = new ArrayList<Node>();
+			children.add(new Node("就业人口人均GDP增长率"+index_321+"%", null));
+			node32.setChildren(children);
+			node3child.add(node32);
+		}
+		String index_331 = goal.getIndex_331();
+		String index_332 = goal.getIndex_332();
+		if (!StringUtils.isEmpty(index_331) || !StringUtils.isEmpty(index_332)) {
+			node33 = new Node();
+			node33.setName("实质性降低年轻人就业率");
+			List<Node> children = new ArrayList<Node>();
+			if (!StringUtils.isEmpty(index_331)) {
+				children.add(new Node(" 年轻人（15-24）就业率"+index_331+"%", null));
+			}
+			if (!StringUtils.isEmpty(index_332)) {
+				children.add(new Node("年轻人（15-24）接受教育、培训和就业人口比例"+index_332+"%", null));
+			}
+			node33.setChildren(children);
+			node3child.add(node33);
+		}
+		String index_341 = goal.getIndex_341();
+		String index_342 = goal.getIndex_342();
+		if (!StringUtils.isEmpty(index_341) || !StringUtils.isEmpty(index_342)) {
+			node34 = new Node();
+			node34.setName("促进可持续旅游业发展，创造就业机会和提升当地文化");
+			List<Node> children = new ArrayList<Node>();
+			if (!StringUtils.isEmpty(index_341)) {
+				children.add(new Node(" 旅游业在GDP中的比例"+index_341+"%", null));
+			}
+			if (!StringUtils.isEmpty(index_342)) {
+				children.add(new Node(" 旅游消费（环境压力指数）"+index_342, null));
+			}
+			node34.setChildren(children);
+			node3child.add(node34);
+		}
+		String index_351 = goal.getIndex_351();
+		String index_352 = goal.getIndex_352();
+		if (!StringUtils.isEmpty(index_351) || !StringUtils.isEmpty(index_352)) {
+			node35 = new Node();
+			node35.setName("确保可持续城市化发展和人人参与能力");
+			List<Node> children = new ArrayList<Node>();
+			if (!StringUtils.isEmpty(index_351)) {
+				children.add(new Node("土地利用率"+index_351+"%", null));
+			}
+			if (!StringUtils.isEmpty(index_352)) {
+				children.add(new Node(" 城镇化率"+index_352+"%", null));
+			}
+			node35.setChildren(children);
+			node3child.add(node35);
+		}
+		String index_361 = goal.getIndex_361();
+		if (!StringUtils.isEmpty(index_361)) {
+			node36 = new Node();
+			node36.setName("社会福利的提高");
+			List<Node> children = new ArrayList<Node>();
+			children.add(new Node("人均社会福利（查用什么指标衡量）"+index_361+"%", null));
+			node36.setChildren(children);
+			node3child.add(node36);
+		}
+		String index_371 = goal.getIndex_371();
+		String index_372 = goal.getIndex_372();
+		String index_373 = goal.getIndex_373();
+		if (!StringUtils.isEmpty(index_371) || !StringUtils.isEmpty(index_372) || !StringUtils.isEmpty(index_373)) {
+			node37 = new Node();
+			node37.setName("提高农业水生产力");
+			List<Node> children = new ArrayList<Node>();
+			if (!StringUtils.isEmpty(index_371)) {
+				children.add(new Node("提高农业水生产力到"+index_371+"%", null));
+			}
+			if (!StringUtils.isEmpty(index_372)) {
+				children.add(new Node("提高农业水利用效率到"+index_372+"%", null));
+			}
+			if (!StringUtils.isEmpty(index_373)) {
+				children.add(new Node("提高每公顷农产品产值"+index_373+"元", null));
+			}
+			node37.setChildren(children);
+			node3child.add(node37);
+		}
+		String index_381 = goal.getIndex_381();
+		String index_382 = goal.getIndex_382();
+		if (!StringUtils.isEmpty(index_381) || !StringUtils.isEmpty(index_382)) {
+			node38 = new Node();
+			node38.setName("确保可持续粮食生产");
+			List<Node> children = new ArrayList<Node>();
+			if (!StringUtils.isEmpty(index_381)) {
+				children.add(new Node("维持可持续发展的中游耕地面积在"+index_381+"万亩", null));
+			}
+			if (!StringUtils.isEmpty(index_382)) {
+				children.add(new Node("农业温室气体排放"+index_382, null));
+			}
+			node38.setChildren(children);
+			node3child.add(node38);
+		}
+		node3.setChildren(node3child);
 		
-		return node1;
+		List<Node> rootchildren = new ArrayList<Node>();
+		rootchildren.add(node1);
+		rootchildren.add(node2);
+		rootchildren.add(node3);
+		root.setName("目标体系设定");
+		root.setChildren(rootchildren);
+		return root;
 	}
 
 }
